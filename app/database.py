44ee8d82488ea46fefa3755649aa8541812d2f01
sylyy <syliasad6@gmail.com>
2024-05-02 14:10:45 +0200

    Added database.py /car.py

--- app/database.py
+++ app/database.py
@@ -1,8 +1,22 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase,sessionmaker
-
 engine = create_engine(
 "sqlite:///data/db.sqlite"
 , # Path to the database file
 echo=True, # Show generated SQL code in the terminal
 )
+Session = sessionmaker(engine)
+
+
+class Base(DeclarativeBase):
+    pass
+
+from app.models.car import Sale,Rental,Purchase,Vehicle,Client,Vendor
+
+
+def create_database():
+    Base.metadata.create_all(engine)
+
+
+def delete_database():
+    Base.metadata.clear()
