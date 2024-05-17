aafad427eff3547ac1604a7a2630ab17e5a67e7b
Anis Boudjadja <anis.boudjadja@student.unamur.be>
2024-05-17 22:59:50 +0200

    05

--- app/routes/cars.py
+++ app/routes/cars.py
@@ -9,6 +9,7 @@ from app.login_manager import login_manager
 from app.schemas import UserSchema
 from fastapi.templating import Jinja2Templates
 from fastapi import Request
+from app.services.users import get_user_by_email
 
 router = APIRouter(prefix="/cars", tags=["Cars"])
 templates = Jinja2Templates(directory="templates")
